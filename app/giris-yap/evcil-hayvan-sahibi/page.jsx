f6c70148453280da29ba2544ae4536560296dd24
Hazar NAMDAR <hazarnamdarrr@gmail.com>
2024-05-18 00:28:44 +0300

    Evcil hayvan login sayfası eklendi

--- app/giris-yap/evcil-hayvan-sahibi/page.jsx
+++ app/giris-yap/evcil-hayvan-sahibi/page.jsx
@@ -3,67 +3,73 @@ import '../../../styles/login.css';
 
 const LogInPetOwner = () => {
   return (
-    <main className='login login--pet-owners'>
+    <>
       <nav className='navbar--pet-owners'>
-        <Image
-          src='/images/logo-hayvanli-beyaz-bg-transparan.png'
-          alt='Logo'
-          width={75}
-          height={50}
-        />
+        <div className='nav-image-wrapper'>
+          <Image
+            src='/images/logo-hayvanli-beyaz-bg-transparan.png'
+            alt='Logo'
+            width={100}
+            height={100}
+          />
+        </div>
       </nav>
+      <main className='login-page login--pet-owners'>
+        <section className='login-container'>
+          <div className='login-container__header'>
+            <div className='header-image-wrapper'>
+              <Image
+                src='/images/logo-hayvanli-turuncu-bg-transparan.png'
+                alt='Pet Owners'
+                width={100}
+                height={100}
+              />
+            </div>
+          </div>
 
-      <section className='login-container'>
-        <div className='login-container__header'>
-          <h1 className='login-container__header-title'>
-            Evcil Hayvan Sahibi Girişi
-          </h1>
-          <p className='login-container__header-subtitle'>
-            Hesabınıza giriş yapın
-          </p>
-        </div>
+          <form className='login-container__form'>
+            <label className='login-container__form-label' htmlFor='email'>
+              E-Mail Adresiniz
+            </label>
+            <input
+              className='login-container__form-input'
+              type='email'
+              id='email'
+              name='email'
+              placeholder='E-posta adresinizi girin'
+              required
+            />
 
-        <form className='login-container__form'>
-          <label className='login-container__form-label' htmlFor='email'>
-            E-Mail Adresiniz
-          </label>
-          <input
-            className='login-container__form-input'
-            type='email'
-            id='email'
-            name='email'
-            placeholder='E-posta adresinizi girin'
-            required
-          />
+            <label className='login-container__form-label' htmlFor='password'>
+              Şifreniz
+            </label>
+            <input
+              className='login-container__form-input'
+              type='password'
+              id='password'
+              name='password'
+              placeholder='Şifrenizi girin'
+              required
+            />
 
-          <label className='login-container__form-label' htmlFor='password'>
-            Şifreniz
-          </label>
-          <input
-            className='login-container__form-input'
-            type='password'
-            id='password'
-            name='password'
-            placeholder='Şifrenizi girin'
-            required
-          />
-
-          <button className='login-container__form-button' type='submit'>
-            Giriş Yap
-          </button>
-        </form>
-
-        <div className='login-container__footer'>
-          <p className='login-container__footer-text'>Hesabınız yok mu?</p>
-          <a
-            className='login-container__form-button login-container__form-button--secondary'
-            href='/kayit-ol/evcil-hayvan-sahibi'
-          >
-            Kayıt Ol
-          </a>
-        </div>
-      </section>
-    </main>
+            <div className='login-container__form-buttons'>
+              <button className='login-container__form-button' type='submit'>
+                Giriş Yap
+              </button>
+              <a
+                className='login-container__form-button purple login-container__form-button--secondary'
+                href='/kayit-ol/evcil-hayvan-sahibi'
+              >
+                Hesabınız yok mu?
+              </a>
+            </div>
+          </form>
+        </section>
+        <section className='login-side-image-wrapper'>
+          <img src='/images/evcil-hayvan-login-resim.jpg' alt='Pet Owners' />
+        </section>
+      </main>
+    </>
   );
 };
 
