d3c9c588e788a50b3fe2d4e537ee201f59f7f040
Hazar NAMDAR <hazarnamdarrr@gmail.com>
2024-05-22 20:20:57 +0300

    modal overlay and modal gizlemesi yapıldı

--- client/src/js/login-signup.js
+++ client/src/js/login-signup.js
@@ -24,8 +24,10 @@ openModal?.addEventListener('click', e => {
   e.preventDefault();
 
   document.querySelector('.overlay').classList.remove('hidden');
+  document.querySelector('.modal').classList.remove('hidden');
 });
 
 closeModal?.addEventListener('click', () => {
   document.querySelector('.overlay').classList.add('hidden');
+  document.querySelector('.modal').classList.add('hidden');
 });
